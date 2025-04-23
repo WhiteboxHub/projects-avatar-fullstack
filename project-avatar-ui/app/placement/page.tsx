f5b1c22473e01ed757fff25ee067163bd5668b85
shivapatel7 <shivapatel098k@gmail.com>
2025-04-23 15:39:45 -0700

    local changes

--- project-avatar-ui/app/placement/page.tsx
+++ project-avatar-ui/app/placement/page.tsx
@@ -705,9 +705,5 @@ const PlacementPage = () => {
         );
     };
     
-    export default PlacementPage;
-
-<<<<<<< HEAD
-=======
 export default PlacementPage;
->>>>>>> 8785e7a (all by vendor routes are working)
+
